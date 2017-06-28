140fdd49a2b15f3dd1eb79f37c7e2a133cfc0db8
Slayera4 <slashera4@gmail.com>
2017-06-28 17:50:04 +0200

    Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import 'font-awesome/css/font-awesome.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
